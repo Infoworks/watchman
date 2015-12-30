3496b00a9626356205928dab19d76a2621ce35d9
Paras Sethia <paras@infoworks.io>
2015-12-30 14:23:56 +0530

    Add: large test for teradata cdc

---
+++ test_suite/teradata_cdc/small/insert/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO type_one (record_add_ts, record_update_ts) VALUES (CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
+INSERT INTO type_two (effective_ts, expiry_ts) VALUES (CURRENT_TIMESTAMP, '2999-12-31 23:59:59')
