7e98f6d64007a1ec3b7a7e7c0079d77e807609b7
Paras Sethia <paras@infoworks.io>
2015-12-30 11:10:52 +0530

    Add: insert-update test configuration

---
+++ test_suite/teradata_cdc/insert_update.sql
@@ -0,0 +1,14 @@
+INSERT INTO type_one (version, record_add_ts, record_update_ts) VALUES (2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP + INTERVAL '1' SECOND)
+INSERT INTO type_one (version, record_add_ts, record_update_ts) VALUES (2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP + INTERVAL '1' SECOND)
+INSERT INTO type_one (version, record_add_ts, record_update_ts) VALUES (2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP + INTERVAL '1' SECOND)
+INSERT INTO type_one (version, record_add_ts, record_update_ts) VALUES (2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP + INTERVAL '1' SECOND)
+INSERT INTO type_one (version, record_add_ts, record_update_ts) VALUES (2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP + INTERVAL '1' SECOND)
+
+INSERT INTO type_two (effective_ts, expiry_ts) VALUES (CURRENT_TIMESTAMP, CURRENT_TIMESTAMP + INTERVAL '1' SECOND)
+INSERT INTO type_two (effective_ts, expiry_ts) VALUES (CURRENT_TIMESTAMP, CURRENT_TIMESTAMP + INTERVAL '1' SECOND)
+INSERT INTO type_two (effective_ts, expiry_ts) VALUES (CURRENT_TIMESTAMP, CURRENT_TIMESTAMP + INTERVAL '1' SECOND)
+INSERT INTO type_two (effective_ts, expiry_ts) VALUES (CURRENT_TIMESTAMP, CURRENT_TIMESTAMP + INTERVAL '1' SECOND)
+INSERT INTO type_two (effective_ts, expiry_ts) VALUES (CURRENT_TIMESTAMP, CURRENT_TIMESTAMP + INTERVAL '1' SECOND)
+
+INSERT INTO type_two (id, version, effective_ts, expiry_ts) SELECT id, max(version)+1, CURRENT_TIMESTAMP + INTERVAL '1' SECOND, '2999-12-31 23:59:59' AS expiry_ts FROM type_two WHERE id > 20 GROUP BY id;
+
