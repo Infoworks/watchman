733c24ae5392f44b95e87aba559f236cd8c3dd35
Paras Sethia <paras@infoworks.io>
2016-01-18 10:19:24 +0530

    Change: history_vw will have only old records

--- code/cdc_validate.py
+++ code/cdc_validate.py
@@ -6,33 +6,41 @@ import json
 
 
 validation_sql = '''
---test1: all rows of current_view are in history_view
+--test1: no rows of current_view are in history_view
 --test2, test3, test4: all dates are derived from the equivalent timestamp values
 --test5: confirm the assumption that ZIW_START_TIME_STAMP < ZIW_END_TIME_STAMP
---test6: only the latest row is marked as active
---test7: no overlapping start-end
+--test6: all rows of history_view are inactive
+--test7: all rows of current_view are active
+--test8: all rows of current_view have unique rowid
+--test9: only the latest row is marked as active
+--test10: no overlapping start-end
 
 WITH
 current_view AS (SELECT * FROM table),
 history_view AS (SELECT * FROM history_vw_table),
-temp1 AS (SELECT count(*) c FROM (SELECT * FROM history_view UNION DISTINCT SELECT * FROM current_view) i1),
+all_view AS (SELECT * FROM current_view UNION DISTINCT SELECT * FROM history_view),
+temp1 AS (SELECT count(*) c FROM all_view),
 temp2 AS (SELECT count(*) c FROM history_view),
-test1 AS (SELECT (temp1.c - temp2.c) c FROM temp1, temp2),
-test2 AS (SELECT count(*) c FROM history_view WHERE cast(ZIW_LOAD_TIME_STAMP AS date) <> ZIW_LOAD_DATE),
-test3 AS (SELECT count(*) c FROM history_view WHERE cast(ZIW_START_TIME_STAMP AS date) <> ZIW_START_DATE),
+temp3 AS (SELECT count(*) c FROM current_view),
+test1 AS (SELECT (temp1.c - temp2.c - temp3.c) c FROM temp1, temp2, temp3),
+test2 AS (SELECT count(*) c FROM all_view WHERE cast(ZIW_LOAD_TIME_STAMP AS date) <> ZIW_LOAD_DATE),
+test3 AS (SELECT count(*) c FROM all_view WHERE cast(ZIW_START_TIME_STAMP AS date) <> ZIW_START_DATE),
 test4 AS (SELECT count(*) c FROM
-    history_view WHERE months_between(cast(ZIW_END_TIME_STAMP AS date), ZIW_END_DATE) <> 0),
-test5 AS (SELECT count(*) c FROM history_view WHERE months_between(ZIW_END_TIME_STAMP, ZIW_START_TIME_STAMP) < 0),
-test6 AS (SELECT count(*) c FROM history_view i1 INNER JOIN
-    (SELECT id, MAX(ZIW_START_TIME_STAMP) ZIW_START_TIME_STAMP FROM history_view i2 GROUP BY id) i2
+    all_view WHERE months_between(cast(ZIW_END_TIME_STAMP AS date), ZIW_END_DATE) <> 0),
+test5 AS (SELECT count(*) c FROM all_view WHERE months_between(ZIW_END_TIME_STAMP, ZIW_START_TIME_STAMP) < 0),
+test6 AS (SELECT count(*) c FROM history_view WHERE ZIW_ACTIVE='true'),
+test7 AS (SELECT count(*) c FROM current_view WHERE ZIW_ACTIVE='false'),
+test8 AS (SELECT count(*) c FROM (SELECT 1 FROM current_view GROUP BY id HAVING count(*) > 1) i1),
+test9 AS (SELECT count(*) c FROM all_view i1 INNER JOIN
+    (SELECT id, MAX(ZIW_START_TIME_STAMP) ZIW_START_TIME_STAMP FROM all_view i2 GROUP BY id) i2
     ON (i1.id=i2.id) WHERE i1.ZIW_ACTIVE='true' AND i1.ZIW_START_TIME_STAMP<i2.ZIW_START_TIME_STAMP),
-temp3 AS (SELECT id, ZIW_START_TIME_STAMP, ZIW_END_TIME_STAMP,
-    row_number() OVER (ORDER BY id, ZIW_START_TIME_STAMP) AS rowid FROM history_view),
-test7 AS (SELECT count(*) c FROM temp3 i1 INNER JOIN temp3 i2 ON
+temp4 AS (SELECT id, ZIW_START_TIME_STAMP, ZIW_END_TIME_STAMP,
+    row_number() OVER (ORDER BY id, ZIW_START_TIME_STAMP) AS rowid FROM all_view),
+test10 AS (SELECT count(*) c FROM temp4 i1 INNER JOIN temp4 i2 ON
     (i1.id=i2.id AND i2.rowid=(i1.rowid+1)) WHERE
     months_between(i1.ZIW_END_TIME_STAMP, i2.ZIW_START_TIME_STAMP) < 0)
-SELECT test1.c, test2.c, test3.c, test4.c, test5.c, test6.c, test7.c FROM
-    test1, test2, test3, test4, test5, test6, test7
+SELECT test1.c, test2.c, test3.c, test4.c, test5.c, test6.c, test7.c, test8.c, test9.c, test10.c FROM
+    test1, test2, test3, test4, test5, test6, test7, test8, test9, test10
 '''
 
 
