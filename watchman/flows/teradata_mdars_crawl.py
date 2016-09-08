a7340704d7d6aa81c8ef2274919416f93d4cd4d3
Thejas <thejas@infoworks.io>
2016-09-08 12:16:05 +0530

    fixing teradata flow

--- watchman/flows/teradata_mdars_crawl.py
+++ watchman/flows/teradata_mdars_crawl.py
@@ -15,7 +15,7 @@ try:
 except KeyError as e:
     file_name = basename(script_name).split('.')[0]
     print 'ROSIE_FLOW_DATASET_BASE_PATH is not set as an env variable.'
-    ROSIE_FLOW_DATASET_BASE_PATH = parent_dir + '/datasets/' + file_name + '/northwind'
+    ROSIE_FLOW_DATASET_BASE_PATH = parent_dir + '/datasets/' + file_name + '/mdars'
     print 'Defaulting to: ' + ROSIE_FLOW_DATASET_BASE_PATH
 
 
@@ -35,9 +35,13 @@ def create_dag():
     Flow for Oracle Northwind end to end
 
     """
+    delete_source_task = PythonOperator(
+        task_id='delete_source', dag=dag,
+        python_callable=delete_source, op_args=[ROSIE_FLOW_DATASET_BASE_PATH + '/delete_source.json'])
+
     source_setup_task = PythonOperator(
         task_id='setup_source', dag=dag, python_callable=source_setup,
-        op_args=[ROSIE_FLOW_DATASET_BASE_PATH + '/dbconf.properties.json',ROSIE_FLOW_DATASET_BASE_PATH + '/fullload.sql'])
+        op_args=[ROSIE_FLOW_DATASET_BASE_PATH + '/dbconf.properties',ROSIE_FLOW_DATASET_BASE_PATH + '/fullload.sql'])
 
     create_source_task = PythonOperator(
         task_id='create_source', dag=dag, python_callable=create_source,
@@ -51,12 +55,13 @@ def create_dag():
         task_id='configure_tables_and_table_groups', dag=dag,
         python_callable=configure_tables_and_table_groups,
         op_args=[ROSIE_FLOW_DATASET_BASE_PATH + '/configure_tables_and_table_groups.json', None,
-                 'create_source', 'source_id'])
+                 'create_source', 'sourceID'])
 
     crawl_table_groups_task = PythonOperator(
         task_id='crawl_table_groups', dag=dag,
         python_callable=crawl_table_groups_from_config, op_args=[ROSIE_FLOW_DATASET_BASE_PATH + '/crawl_data.json'])
 
+    source_setup_task.set_upstream(delete_source_task)
     create_source_task.set_upstream(source_setup_task)
     crawl_metadata_task.set_upstream(create_source_task)
     configure_tables_and_table_groups_task.set_upstream(crawl_metadata_task)
