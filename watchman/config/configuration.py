57b781279122e77664367c3827245259559d7d50
Abhishek <abhishek@infoworks.io>
2016-09-08 11:26:22 +0530

    fix: auth token

--- watchman/config/configuration.py
+++ watchman/config/configuration.py
@@ -2,7 +2,7 @@ import os
 
 HOST = os.getenv('ROSIE_FLOW_IW_HOST', default='54.174.145.91')
 
-AUTH_TOKEN = os.getenv('ROSIE_FLOW_IW_USER_AUTH_TOKEN', default='YWRtaW5AaW5mb3dvcmtzLmlvOjEyMzQ1Ng==')
+AUTH_TOKEN = os.getenv('ROSIE_FLOW_IW_USER_AUTH_TOKEN', default='aW5mb3dvcmtzOmFkbWluQGluZm93b3Jrcy5pbzoxMjM0NTY=')
 
 
 REST_HOST = HOST
